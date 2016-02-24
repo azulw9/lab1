cba45119563dc28c5ccaf66187c5a9c944ef6396
azulw9 <azulw9@gmail.com>
2016-02-24 19:52:19 +0200

    add comments func.cpp

--- func.cpp
+++ func.cpp
@@ -2,11 +2,11 @@
 
 void Rand_x_modp (mpz_t x, mpz_t p, mpz_t n, unsigned long c, int m2exp) //c, m2exp менять перед новым вызовом
 {
-     mpz_t temp; mpz_init(temp);
+     //mpz_t temp; mpz_init(temp);
       //gmp_randstate_t state; 
-      gmp_randinit_lc_2exp(state, n, c, m2exp);
-      //mpz_urandomb(n,state,257);
-	  mpz_urandomb(n,state,256);
+      ///gmp_randinit_lc_2exp(state, n, c, m2exp);
+      ////mpz_urandomb(n,state,257);
+	 /// mpz_urandomb(n,state,256);
       
 }
 
