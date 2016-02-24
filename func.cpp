b84ded10d9b1d3be9a006135e2cb2f975639da9f
azulw9 <azulw9@gmail.com>
2016-02-24 19:03:51 +0200

    changed_func.cpp

--- func.cpp
+++ func.cpp
@@ -6,7 +6,7 @@ void Rand_x_modp (mpz_t x, mpz_t p, mpz_t n, unsigned long c, int m2exp) //c, m2
       gmp_randstate_t state; 
       gmp_randinit_lc_2exp(state, n, c, m2exp);
       mpz_urandomb(n,state,257);
-	  //mpz_urandomb(n,state,256);
+	  mpz_urandomb(n,state,256);
       
 }
 
